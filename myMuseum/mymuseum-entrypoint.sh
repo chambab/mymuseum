46ec61f6e537778ffaeacc2c2b0843fcd98f6d78
chambab <chambab@gmail.com>
2019-09-28 12:15:15 +0900

    set up additional parameter

---
+++ myMuseum/mymuseum-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i "s/pgsql_host=/pgsql_host=$PGSQL_HOST/g" /usr/local/tomcat/webapps/ROOT/webapp/WEB-INF/classes/context.properties
+sed -i "s/pgsql_port=/pgsql_port=$PGSQL_PORT/g" /usr/local/tomcat/webapps/ROOT/webapp/WEB-INF/classes/context.properties
+sed -i "s/pgsql_dbname=/pgsql_dbname=$PGSQL_DBNAME/g" /usr/local/tomcat/webapps/ROOT/webapp/WEB-INF/classes/context.properties
+sed -i "s/pgsql_id=/pgsql_id=${PGSQL_ID}/g" /usr/local/tomcat/webapps/ROOT/webapp/WEB-INF/classes/context.properties
+sed -i "s/pgsql_pw=/pgsql_pw=$PGSQL_PW/g" /usr/local/tomcat/webapps/ROOT/webapp/WEB-INF/classes/context.properties
+
+exec "$@"
